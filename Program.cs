1a44008afaba06a72f843328eb55b762449be8b9
VinogradovMihail <misha-vinogradov-1997@mail.ru>
2022-09-06 17:28:50 +0300

    Task001 done

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//  Написать программу, которая из имеющегося массива строк формирует 
+// новый массив из строк, длина которых меньше, либо равна 3 символам.
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void StringsLessThenThree(string[] array1, string[] array2)
+{
+    int index = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3)
+        {
+            array2[index] = array1[i];
+            index++;
+        }
+    }
+}
+
+string[] array3 = {"Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"};
+string[] array4 = new string[array3.Length];
+
+StringsLessThenThree(array3, array4);
+PrintArray(array4);
+
